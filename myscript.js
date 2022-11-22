f558191027d76587ab296b779236d6e0945e6b05
z kim macie praktyki? <94898743+dopkelo@users.noreply.github.com>
2022-11-22 13:01:52 +0100

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,115 +1,31 @@
-function Calculate(wynik) 
-{
-    var a = document.getElementById('a').value;
-    var b = document.getElementById('b').value;  
- 
-    switch(wynik) 
-    {
-        case '+':
-            var result = parseFloat(a) + parseFloat(b);         
-        break;
-        case '-':
-            var result = parseFloat(a) - parseFloat(b);         
-        break;  
-        case 'x':
-            var result = parseFloat(a) * parseFloat(b);         
-        break;  
-        case '/':
-            var result = parseFloat(a) / parseFloat(b);         
-        break;  
-        case 'pow':
-            var result = potega(parseFloat(a),parseFloat(b));
-            break;
-         case 'log':
-            var result = log2(parseFloat(a) );
-            break;
-           
+const books = [
+    {title: 'Total loss', pages: 600, genre: 'fantasy', rating: 5},
+    {title: 'Total enlightenment', pages: 250, genre: 'romance', rating: 2},
+    {title: 'Big loss', pages: 400, genre: 'fantasy', rating: 7},
+    {title: '10th Joy', pages: 32, genre: 'action', rating: 8},
+    {title: 'Quickfix', pages: 15, genre: 'fantasy', rating: 1},
+    {title: 'World Ender', pages: 199, genre: 'fantasy', rating: 3},
+    {title: 'Paranormal', pages: 200, genre: 'thriller', rating: 9},
+    {title: '300', pages: 600, genre: 'criminology', rating: 10},
+    {title: 'Renewer', pages: 472, genre: 'biology', rating: 2},
+];
+const compose = (...fns) => (x) => fns.reduceRight((acc, fn) => fn(acc), x);
 
-    }
- 
-    document.getElementById('result').value = result;
-}
-function handlePalindromeForm(form){
+const brakSpacji = (list) => list.filter((letter)=>letter !=' ');
+const parzysteStrony = (list) => list.filter((book) => book.pages % 2 == 0);
+const koniecY = (list) => list.filter((book) => book.genre.endsWith('y'));
+const dlugoscTablicy = (list) => list.length;
+const samTytul = (book) => book.map((a)=> a.title);
+const polaczenie = (list) => list.reduce((a, b) => [...a, ...b]);
+const zad6= compose(dlugoscTablicy,brakSpacji,polaczenie,samTytul,parzysteStrony,koniecY);
+console.log(zad6(books));
+const nieParzysteStrony = (list) => list.filter((book) => book.pages % 2 == 1);
+const pozytywneOceny = (list) => list.filter((a) => a.rating>5).length;
+const tytulZLiczba = (list) => list.filter((a) =>/\d/.test(a.title));
+const zad7= compose(pozytywneOceny,nieParzysteStrony,tytulZLiczba);
+console.log(zad7(books));
+const drugiTytul = (title) => title.sort((a,b) => b.length -a.length).at(1);
+const zad8= compose(drugiTytul,samTytul);
+console.log(zad8(books));
 
-    let formValue = form.inputbox2.value;
-    document.getElementById("palindromeResult").innerHTML = palindrome(formValue);
-}
-function handlePalindrome(form) {
-    form.result.innerHTML = palindrome(form.input.value);
-}
 
-function palindrome(a) {
-    a = a.toLowerCase();
-    for(let i in a) {
-        if(a[i] != a[a.length - 1 - i])
-            return false;
-    }
-    return true;
-}
-
-function sprawdzString(array){
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        if(typeof element =="string"){
-            return true
-        }
-        
-      }
-      return false
-}
-function przedostatni(array){
-    if(sprawdzString(array)==false){
-        let najwiekszy = 0;
-        let predostatni = 0;
-        for (let index = 0; index < array.length; index++) {
-            const element = array[index];
-            if(element>najwiekszy){
-                predostatni = najwiekszy;
-                najwiekszy = element;
-
-            }
-        }
-        return predostatni;
-    } else {
-        let najwiekszys ="";
-        let predostatnis="";
-        for (let index = 0; index < array.length; index++) {
-            const element = array[index];
-            if(element>najwiekszys){
-                predostatnis = najwiekszys;
-                najwiekszys = element;
-
-            } 
-        }
-        return predostatnis;
-    }
-}
-function handleAnagramForm(form){
-
-    let a = form.inputbox3.value;
-    let b = form.inputbox4.value;
-    document.getElementById("anagramResult").innerHTML = przedostatni(a,b);
-}
-function rekurencja(a,b){
-    if(a<b){
-        return String(a) +" "+ String(rekurencja(a+1,b))
-    }else{
-    return a
-    }
-}
-   function handlerekurencjaForm(form){
-    let a = parseInt(form.inputbox5.value);
-    let b = parseInt(form.inputbox6.value);
-    document.getElementById("rekurencjaResult").innerHTML = rekurencja(a,b);
-   }
-   function potega(a,b){
-    if(b!==0)
-    return a * potega(a, b-1);
-    return 1;
-
-   }
-   function log2(a){
-    if(a>1)
-    return 1 + log2(a/2);
-    return 0;
-   }
